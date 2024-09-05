4d8e407e9f01e77eb90ddd7908c52480a6a7efde
Renato2210 <renatoastargo04@gmail.com>
2024-09-05 14:29:33 -0400

    FIrst

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TeLlevoApp_AGV',
+  webDir: 'www'
+};
+
+export default config;
